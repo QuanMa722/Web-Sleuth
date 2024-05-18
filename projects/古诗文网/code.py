864e0665acd1edc729a2a8370cb9cb9c6e7eba7e
QuanMa722 <663244693@qq.com>
2024-05-18 12:48:10 +0800

    update

--- projects/古诗文网/code.py
+++ projects/古诗文网/code.py
@@ -8,44 +8,35 @@ import time
 driver = webdriver.Edge()
 
 
-def get_infor():
+def get_data():
+
     url = "http://www.hxlib.cn/book/a519487a2ececa4eedee995d126c745b.html"
     driver.get(url)
 
-    for i in range(0, 800):
-        driver.switch_to.window(driver.window_handles[i])
+    for index in range(0, 800):
+        driver.switch_to.window(driver.window_handles[index])
+
         time.sleep(3)
-        find_text1 = driver.find_element(By.XPATH, '//*[@id="Ls"]/div[2]/div/div/div[1]/ul/li[2]/p/font/b/span/a')
-        find_text1.click()
+
+        find_text = driver.find_element(By.XPATH, '//*[@id="Ls"]/div[2]/div/div/div[1]/ul/li[2]/p/font/b/span/a')
+        find_text.click()
 
         find_text = driver.find_element(By.XPATH, '//*[@id="Ls"]/div[2]/div/div/div[1]/div/font/div')
 
-        new_txt1 = ""
-        try:
-            for i in find_text.text:
-                try:
-                    print(i)
-                    new_txt1 += str(i)
-                except Exception as error_word:
-                    print(f"Error: {error_word}. Skipping this word.")
-        except Exception as error:
-            print(f"An error occurred: {error}")
+        new_txt = ""
 
-        print(new_txt1)
-        get_file(new_txt1)
+        for item in find_text.text:
+            try:
+                new_txt += str(item)
+            except Exception as error_word:
+                print(f"Error: {error_word}. Skipping this word.")
 
-
-def get_file(new_text):
-
-    with open("book.txt", "a", encoding="utf-8") as f:
-        f.write(new_text + "\n")
-
-    return None
+        print(new_txt)
 
 
 if __name__ == '__main__':
     try:
-        get_infor()
+        get_data()
     except Exception as e:
         print(f"An error occurred: {e}")
 
