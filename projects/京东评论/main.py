2a997e053cafc6627966b43b49368ff6832d1056
Quan Ma <663244693@qq.com>
2024-07-07 22:59:46 +0800

    Update main.py

--- projects/京东评论/main.py
+++ projects/京东评论/main.py
@@ -6,12 +6,13 @@ import asyncio
 import time
 
 # product_id = '100066896356'
-# comment_fetcher = CommentFetcher(product_id)
+# file_path = 'comments.csv'
+# comment_fetcher = CommentFetcher(file_path, product_id)
 # asyncio.run(comment_fetcher.main())
 
-# goods list
+
 file_path = 'comments.csv'
-product_id_list: list = ['100015381842', '100039032489', '100112475212', '100047641695', '100097484090']
+product_id_list: list = ['100015381842', '100039032489', '100112475212', '100047641695', '100097484090']  # product list
 
 for product_id in product_id_list:
     comment_fetcher = CommentFetcher(file_path, product_id)
