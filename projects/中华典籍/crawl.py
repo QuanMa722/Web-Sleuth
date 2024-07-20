a40b65f0e7a28591f417abe77617433a5cbd5001
Quan Ma <663244693@qq.com>
2024-07-20 18:47:33 +0800

    Update crawl.py

--- projects/中华典籍/crawl.py
+++ projects/中华典籍/crawl.py
@@ -1,24 +1,37 @@
 # -*- coding: utf-8 -*-
 
+from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 import aiohttp
 import asyncio
+import time
 import os
+
+ua = UserAgent()
+headers = {
+    'User-Agent': ua.random
+}
+
+
+def printt(msg):
+    nowt = time.strftime("%H:%M:%S", time.localtime(int(time.time())))
+    msgs = msg.split("\n")
+    for word in msgs:
+        print("[" + nowt + "] " + str(word))
 
 
 async def fetch(session, url):
     try:
-        async with session.get(url) as response:
+        async with session.get(url=url, headers=headers) as response:
             if response.status == 200:
                 html_text = await response.text()
                 soup = BeautifulSoup(html_text, 'html.parser')
 
                 title: str = soup.find('h1').get_text().strip()
                 text: str = soup.find('div', {'id': 'content', 'class': 'panel-body'}).get_text().strip()
-                print(title)
+                printt(title)
 
                 os.makedirs("ming", exist_ok=True)
-
                 filename = os.path.join("ming", str(title) + '.txt')
                 with open(filename, mode="w", encoding="utf-8") as f:
                     f.write(text)
@@ -30,6 +43,7 @@ async def fetch(session, url):
 
 
 async def main():
+    start_time = time.time()
     async with aiohttp.ClientSession() as session:
         page_list = range(4380, 4713)
         split_pages = [list(page_list)[i:i + 20] for i in range(0, len(page_list), 20)]
@@ -38,11 +52,16 @@ async def main():
             await asyncio.sleep(10)
             tasks = []
             for page in pages:
+                # Change the url and directory of the file to be captured.
                 url = f"https://www.zhonghuadiancang.com/lishizhuanji/mingshi/{page}.html"
                 task = fetch(session, url)
                 tasks.append(task)
 
             await asyncio.gather(*tasks)
+
+    end_time = time.time()
+    total_time = round((end_time - start_time), 2)
+    printt(f"Total running time: {total_time} seconds")
 
 if __name__ == '__main__':
     asyncio.run(main())
